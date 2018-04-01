8060a84b68dbf51464c9a9252effff16d4d98649
Cameron Cruz <cameroncruz@DN51utb0.SUNet>
2018-04-01 15:54:14 -0700

    Finish site except youtube embed

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 window.onload = function() {
   console.log("Init ClipboardJS");
   new ClipboardJS('.cpy-clipboard');
+
+  var setTooltip = function() {
+    this.setAttribute("uk-tooltip", "title: Copied!; pos: top-right");
+    this.style.opacity = 0.54;
+};
+
+  Array.from(document.getElementsByClassName('cpy-clipboard')).forEach(function(element) {
+      element.addEventListener('click', setTooltip);
+    });
 };
